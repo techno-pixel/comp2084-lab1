c1575f50deb8a3a8345a7b56c96e79cf31c43dfb
techno-pixel <jseaidoun@gmail.com>
2021-01-19 10:01:03 -0500

    completing Lab 1

--- Startup.cs
+++ Startup.cs
@@ -20,7 +20,7 @@ namespace Lab1
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container. Test
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
